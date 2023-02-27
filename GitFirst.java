1a34c43e7e3b68d14bfee4ca695fea3c329af13f
navinkumar2995 <snk.king95@gmail.com>
2023-02-27 11:00:14 +0530

    message

---
+++ GitFirst.java
@@ -0,0 +1,22 @@
+package org.hub;
+
+public class GitFirst {
+	public static void main(String[] args) {
+		System.out.println("First");
+	}
+<<<<<<< HEAD
+	private void navisha() {
+		System.out.println("navisha completed");
+=======
+<<<<<<< HEAD
+	private void navin() {
+		System.out.println("navin completed");
+=======
+	private void anu() {
+		System.out.println("anu completed");
+>>>>>>> e1d18d330f13a9c5907542a74b32585a6766b048
+>>>>>>> 144656dbad44c44ecbd4dbed3be48896bab4d684
+
+	}
+
+}
